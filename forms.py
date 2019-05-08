265c49d5e8edec3f73dd0363047ccc0a2498ff74
Oscar Machado Quintela <oscarmachadoquintela@gmail.com>
2019-05-08 21:16:39 +0200

    Add register and login routes and templates

---
+++ forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+
+    username = StringField(label='Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField(label='Email',
+                        validators=[DataRequired(), Email()])
+
+    password = StringField(label='Password',
+                           validators=[DataRequired()])
+
+    confirm_password = StringField(label='Confirm Password',
+                                   validators=[DataRequired(), EqualTo('password', message='')])
+
+    submit = SubmitField(label='Sign Up')
+
+
+class LoginForm(FlaskForm):
+
+    email = StringField(label='Email',
+                        validators=[DataRequired(), Email()])
+
+    password = StringField(label='Password',
+                           validators=[DataRequired()])
+
+    remember = BooleanField(label='Remember Me')
+
+    submit = SubmitField(label='Login')
--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 from datetime import datetime
+from config import SECRET_KEY
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 posts = [
     {
@@ -27,6 +31,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
