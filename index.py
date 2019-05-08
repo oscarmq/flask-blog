65c99e8d6e18f5577fdd73bf31985e57f1249bbf
Oscar Machado Quintela <oscarmachadoquintela@gmail.com>
2019-05-08 20:09:21 +0200

    Create templates
    Home and about templates inherit from layout. Add css file and
    bootstrap.

--- index.py
+++ index.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from datetime import datetime
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Oscar Machado',
+        'title': 'Blog post 1',
+        'content': 'First post',
+        'day_posted': datetime.now()
+    },
+    {
+        'author': 'Carlos Machado',
+        'title': 'Blog post 1',
+        'content': 'First post',
+        'day_posted': datetime.now()
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
