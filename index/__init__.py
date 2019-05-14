940e7afaf1c8a1f675077bb84769042c17ac9085
Oscar Machado Quintela <oscarmachadoquintela@gmail.com>
2019-05-14 18:51:56 +0200

    Add account route
    Users has to be logged in to see the account page.

--- index/__init__.py
+++ index/__init__.py
@@ -11,5 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.message_category = 'info'
 
 from index import routes
--- index/routes.py
+++ index/routes.py
@@ -2,7 +2,7 @@ from index import app, bcrypt, db
 from flask import render_template, url_for, flash, redirect
 from index.forms import RegistrationForm, LoginForm
 from index.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
 
 posts = [
@@ -73,3 +73,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
