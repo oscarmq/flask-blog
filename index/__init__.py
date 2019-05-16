121168a6fff4b5c8dbbe56a2410bd610e91f0dff
oscarmq <oscarmachadoquintela@gmail.com>
2019-05-16 18:17:34 +0200

    Load the config variables from the ConfigManager
    Config variables were move to enviroment variables.

--- index/__init__.py
+++ index/__init__.py
@@ -2,15 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from index.config import ConfigManager
 
 def create_app():
     pass
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET_KEY'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(ConfigManager)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
