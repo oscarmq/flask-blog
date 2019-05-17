8912283d376b24cf59e18bac119e0dd0500fe46e
oscarmq <oscarmachadoquintela@gmail.com>
2019-05-17 10:54:59 +0200

    Create the app at runtime
    Creates the app at runtime using the application
    factory function create_app(). This function returns
    a configured app instance.

--- index/__init__.py
+++ index/__init__.py
@@ -4,22 +4,29 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from index.config import ConfigManager
 
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+
 def create_app():
-    pass
 
+    app = Flask(__name__)
+    app.config.from_object(ConfigManager)
 
-app = Flask(__name__)
-app.config.from_object(ConfigManager)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'users.login'
+    login_manager.login_message_category = 'info'
 
-from index.main.routes import main
-from index.users.routes import users
-from index.posts.routes import posts
+    from index.main.routes import main
+    from index.users.routes import users
+    from index.posts.routes import posts
 
-app.register_blueprint(main)
-app.register_blueprint(users)
-app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+
+    return app
